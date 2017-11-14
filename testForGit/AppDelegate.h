bb88f9ae4e9ae11990711a492c761ecc3b44bd57
yanyuyunyuan <781073103@qq.com>
2017-11-14 15:57:15 +0800

    Initial Commit

---
+++ testForGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testForGit
+//
+//  Created by Mac on 2017/11/14.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
